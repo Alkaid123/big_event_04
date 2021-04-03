55db43c2e25b5baa57b3b62a45c93020891cd79c
88 <1546800185@qq.com>
2021-04-03 15:55:43 +0800

    主页功能完成

--- assets/js/baseAPI.js
+++ assets/js/baseAPI.js
@@ -4,5 +4,25 @@ $(function () {
 
     $.ajaxPrefilter(function (options) {
         options.url = baseURL + options.url;
+
+        // 如果是 /my/ 开头的，要加上 Authorization
+        if (options.url.indexOf('/my/') != -1) {
+            options.headers = {
+                Authorization: localStorage.getItem('token') || '',
+            },
+
+                // 登录拦截
+                options.complete = function (res) {
+                    // console.log(res.responseJSON);
+                    let obj = res.responseJSON;
+                    // 判断：如果 status 等于 1 且 message 
+                    if (obj.code == 1 && obj.message === "身份认证失败！") {
+                        // 清空本地token
+                        localStorage.removeItem('token');
+                        // 跳转到 login.html
+                        location.href = '/login.html';
+                    }
+                }
+        }
     });
 })
---
+++ assets/js/index.js
@@ -0,0 +1,55 @@
+// 入口函数
+$(function () {
+    // 1、用于获取用户信息
+    getUserInfo();
+
+    // 2、退出
+    $('#btnLogout').on('click', function () {
+        // 询问
+        layer.confirm('确定退出登录？', { icon: 3, title: '提示' }, function (index) {
+            //清空token值，跳转到登录页
+            localStorage.removeItem('token');
+            location.href = '/login.html';
+            layer.close(index);
+        });
+    });
+
+
+});
+
+// 用户信息，全局函数
+function getUserInfo() {
+    // 发送ajax请求
+    $.ajax({
+        url: '/my/userinfo',
+        type: 'GET',
+        data: {},
+        success: (res) => {
+            // console.log(res);
+            // 请求失败
+            if (res.status != 0) {
+                return layer.msg(res.msg, { icon: 5 });
+            }
+
+            // 请求成功，渲染头像
+            renderAvatar(res.data)
+        }
+    });
+};
+
+// 渲染头像
+function renderAvatar(user) {
+    // 1、渲染名称（nickname优先，如果没有，就用username）
+    let name = user.nickname || user.username;
+    $('#welcome').html(`欢迎 &nbsp;&nbsp;${name}`);
+    // 2、渲染头像 如果用户上传了头像 则用头像，如果没有，则用名字首字母大写
+    if (user.user_pic) {
+        // 有头像
+        $('.layui-nav-img').attr('src', user.user_pic);
+        $('.avatar').hide();
+    } else {
+        let text = name[0].toUpperCase();
+        $('.layui-nav-img').hide()
+        $('.avatar').show().html(text);
+    }
+}
